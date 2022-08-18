c46deeaa475beef553269abf84346bb227468fcd
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-08-18 17:03:29 +0200

    some adds

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -6,23 +6,49 @@
 </template>
 
 <script setup>
+mport { ref, computed } from "vue";
 import PersonalRouter from "./PersonalRouter.vue";
+import { supabase } from "../supabase";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
+import { storeToRefs } from "pinia";
 
 // Route Variables
 const route = "/auth/login";
-const buttonText = "Test the Sign In Route";
+const buttonText = "Test the SignUp In Route";
 
 // Input Fields
+const email = ref("");
+const password = ref("");
 
 // Error Message
+const errorMsg = ref("");
 
 // Show hide password variable
-
+const passwordFieldType = computed(() =>
+  hidePassword.value ? "password" : "text"
+);
 // Show hide confrimPassword variable
+const hidePassword = ref(true);
 
 // Router to push user once SignedUp to Log In
-
+const redirect = useRouter();
 // Arrow function to SignUp user to supaBase with a timeOut() method for showing the error
+const signUp = async () => {
+  try {
+    // calls the user store and send the users info to backend to logIn
+    await useUserStore().signUp(email.value, password.value);
+    // redirects user to the homeView
+    redirect.push({ path: "/auth" });
+  } catch (error) {
+    // displays error message
+    errorMsg.value = `Error: ${error.message}`;
+    // hides error message
+    setTimeout(() => {
+      errorMsg.value = null;
+    }, 5000);
+  }
+};
 </script>
 
 <style></style>
